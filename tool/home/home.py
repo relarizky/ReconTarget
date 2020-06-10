0571d5571b24bc6a96efa4efc8e938d61810f810
relarizky <relarizky19@gmail.com>
2020-06-10 22:52:14 +0700

    fix show list target filtered by user id

--- tool/home/home.py
+++ tool/home/home.py
@@ -12,7 +12,7 @@ from .user import *
 @login_required
 def index():
     page = request.args.get('page', 1, type = int)
-    target = Target.query.paginate(page, 5, False)
+    target = Target.query.filter_by(id_user = current_user.id).paginate(page, 5, False)
     return render_template('home/index.html', targets = target)
 
 

c26da8c3186d593c27012f6012ec68a1b3fbe859
relarizky <relarizky19@gmail.com>
2020-06-23 00:03:12 +0700

    finished link scrapper (scan, download, view)

--- tool/home/link_scrapper.py
+++ tool/home/link_scrapper.py
@@ -17,7 +17,7 @@ def link_scrapper_index():
     return render_template('home/linkscrapper/list_target.html', targets = link)
 
 
-@home_bp.route('/linkscrapper/scan/<int:id>')
+@home_bp.route('/linkscrapper/scan/<int:id>', methods = ['GET', 'POST'])
 @login_required
 def link_scrapper_scan(id):
     target = Target.query.get(id)
@@ -44,12 +44,15 @@ def link_scrapper_scan(id):
             flash('error', 'fail to scan bcz, {}'.format(Error))
             return secure_redirect(request.headers.get('referer'), url_for('home.link_scrapper_index'))
 
-        if found_link != None:
+        if len(found_link) != 0:
             if target.foundlink.first() == None:
                 targetlink = FoundLink(target, found_link)
             else:
                 targetlink = FoundLink.query.get(target.foundlink.first().id)
-                targetlink.found_link = found_link
+                old_link = targetlink.found_link
+                new_link = set(old_link + found_link)
+                new_link = list(new_link)
+                targetlink.found_link = new_link
             db.session.add(targetlink)
             db.session.commit()
         else:
@@ -58,6 +61,64 @@ def link_scrapper_scan(id):
 
         flash('success', 'Found {} links in {}'.format(str(len(found_link)), target.target_url))
         return secure_redirect(request.headers.get('referer'), url_for('home.link_scrapper_index'))
+    else:
+        flash('error', 'Cant find target with id {}'.format(str(id)))
+        return redirect(url_for('home.link_scrapper_index'))
+
+
+@home_bp.route('/linkscrapper/download/<int:id>')
+@login_required
+def link_scrapper_download(id):
+    tempfile.tempdir = os.getcwd()
+    target = Target.query.get(id)
+
+    if target != None:
+        if target.id_user != current_user.id:
+            flash('error', 'You are not allowed to do that!')
+            return redirect(url_for('home.link_scrapper_index'))
+
+        if target.foundlink.first() == None:
+            flash('error', 'You have not done any scan to this target')
+            return redirect(url_for('home.link_scrapper_index'))
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            tempfile.tempdir = temp_dir
+            file_name = 'link_' + get_info(target.target_url, info = 'domain') + '.txt'
+            file_object = tempfile.NamedTemporaryFile(mode = 'w+t')
+            file_content = target.foundlink.first().found_link
+
+            try:
+                file_object.writelines('\n'.join(file_content))
+                file_object.seek(0)
+                response = send_file(file_object.name,
+                    mimetype = 'text/plain',
+                    as_attachment = True,
+                    attachment_filename = file_name)
+                response.headers['Content-Length'] = os.path.getsize(file_object.name)
+                response.headers['Cache-Control'] = 'no-cache'
+            finally:
+                file_object.close()
+
+            return response
+    else:
+        flash('error', 'Cant find target with id {}'.format(str(id)))
+        return redirect(url_for('home.link_scrapper_index'))
+
+
+@home_bp.route('/linkscrapper/view/<int:id>')
+@login_required
+def link_scrapper_view_raw(id):
+    target = Target.query.get(id)
+
+    if target != None:
+        if target.id_user != current_user.id:
+            flash('error', 'You are not allowed to do that!')
+            return redirect(url_for('home.link_scrapper_index'))
+        elif target.foundlink.first() == None:
+            flash('error', 'You have not done any scan to this target')
+            return redirect(url_for('home.link_scrapper_index'))
+        else:
+            return '<br>'.join(target.foundlink.first().found_link)
     else:
         flash('error', 'Cant find target with id {}'.format(str(id)))
         return redirect(url_for('home.link_scrapper_index'))

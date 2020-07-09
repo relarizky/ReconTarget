994f1f50f4f3c9ec3cf9fa51dc361d550d3520a9
relarizky <relarizky19@gmail.com>
2020-07-09 13:01:17 +0700

    fixed error in run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from app import app
 
 if __name__ == '__main__':
-    config = app.config.get('CONFIG')
+    config = os.getcwd() + '/config.json'
 
     if os.path.exists(config) == False:
         print('[!] Config file is not found.')
         os._exit(1)
-    else:
-        app.run()
+
+    from app import app
+    app.run()
